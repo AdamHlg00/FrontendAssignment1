aa6b622a6f3a3b36bfd67d29cc788ff58865bcbd
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-03-15 16:16:31 +0100

    Added functional sorting, created displayBooks for the purpose of displaying the books

--- js/main.js
+++ js/main.js
@@ -2,20 +2,106 @@ import '../scss/style.scss'
 import { getJSON } from './utils/getJSON.js'
 import { imageHelper } from './utils/imageHelper'
 
-async function start() {
-  let books = await getJSON('/json/books.json')
+let chosenSortOption = 'Title (Ascending)',     // Default sort option
+  books
 
+async function start() {
+  books = await getJSON('/json/books.json')
+
+  addSortingOptions()
+  displayBooks()
+}
+
+function addSortingOptions() {
+  document.querySelector('.sortingOptions').innerHTML = /*html*/`
+    <label><span>Sort by:</span>
+      <select class="sortOption">
+        <option>Title (Ascending)</option>
+        <option>Title (Descending)</option>
+        <option>Price (Ascending)</option>
+        <option>Price (Descending)</option>
+        <option>Author (Ascending)</option>
+        <option>Author (Descending)</option>
+      </select>
+    </label>
+  `
+
+  document.querySelector('.sortOption').addEventListener('change', event => {
+    chosenSortOption = event.target.value
+    displayBooks()
+  })
+}
+
+function sortByTitleAsc(books) {
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+    aTitle > bTitle ? 1 : -1)
+}
+
+function sortByTitleDesc(books) {
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+    aTitle > bTitle ? -1 : 1)
+}
+
+function sortByPriceAsc(books) {
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+    aPrice > bPrice ? 1 : -1)
+}
+
+function sortByPriceDesc(books) {
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+    aPrice > bPrice ? -1 : 1)
+}
+
+function sortByAuthorAsc(books) {
+  books.sort(({ author: aAuthor }, { author: bAuthor }) =>
+    aAuthor > bAuthor ? 1 : -1)
+}
+
+function sortByAuthorDesc(books) {
+  books.sort(({ author: aAuthor }, { author: bAuthor }) =>
+    aAuthor > bAuthor ? -1 : 1)
+}
+
+// Displays books
+function displayBooks() {
   let html = ''
+  let filteredBooks = books     // Not filtered yet!
+
+  if (chosenSortOption === 'Title (Ascending)') {
+    sortByTitleAsc(filteredBooks)
+  }
+
+  if (chosenSortOption === 'Title (Descending)') {
+    sortByTitleDesc(filteredBooks)
+  }
+
+  if (chosenSortOption === 'Price (Ascending)') {
+    sortByPriceAsc(filteredBooks)
+  }
+
+  if (chosenSortOption === 'Price (Descending)') {
+    sortByPriceDesc(filteredBooks)
+  }
+
+  if (chosenSortOption === 'Author (Ascending)') {
+    console.log('hej')
+    sortByAuthorAsc(filteredBooks)
+  }
+
+  if (chosenSortOption === 'Author (Descending)') {
+    sortByAuthorDesc(filteredBooks)
+  }
+
   for (let book of books) {
     // Displays each books image
     let image = imageHelper(book.title)
-    html += `<img class="bookImage" src="${image}" alt="Image of a book">`
+    html += /*html*/`<img class="bookImage" src="${image}" alt="Image of a book">`
 
     // Displays the information of each book
     html += '<div class="book">'
     for (let key in book) {
       let value = book[key]
-      html += `<p><span>${key}:</span> ${value}</p>`
+      html += /*html*/`<p><span>${key}:</span> ${value}</p>`
     }
     html += '</div>'
   }
